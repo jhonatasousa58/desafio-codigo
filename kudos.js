4e014e034317d3780bf8ea97b72dbb430f1ba9ab
Adelson Jhonata <jhonatasousa58@gmail.com>
2021-06-30 21:22:23 -0300

    Primeira Implementacao, esta funcional, mas vou tentar otimizar um pouco mais

--- kudos.js
+++ kudos.js
@@ -1,3 +1,7 @@
+//var extenso = require('extenso')
+
+const extenso = require("extenso");
+
 // Conversão de kudos para pontos
 const KUDOS_TO_POINTS = [
   { name: 'OK', value: 5 },
@@ -20,13 +24,54 @@ const KUDOS_TO_REAL = [
   Recebe: um int representando o número de pontos do usuário
   Retorna: um array contendo os kudos. Ex.: ['OK', 'GOOD'] 
 */
-function getKudosForUser(points) {}
+function getKudosForUser(points) {
+  var kudos = [];
+  while(points > 1) {
+    if(points >= KUDOS_TO_POINTS[4].value){
+      kudos.push(KUDOS_TO_POINTS[4].name);
+      points -= KUDOS_TO_POINTS[4].value;
+    }else if(points >= KUDOS_TO_POINTS[3].value){
+      kudos.push(KUDOS_TO_POINTS[3].name);
+      points -= KUDOS_TO_POINTS[3].value;
+    }else if(points >= KUDOS_TO_POINTS[2].value){
+      kudos.push(KUDOS_TO_POINTS[2].name);
+      points -= KUDOS_TO_POINTS[2].value;
+    }else if(points >= KUDOS_TO_POINTS[1].value){
+      kudos.push(KUDOS_TO_POINTS[1].name);
+      points -= KUDOS_TO_POINTS[1].value;
+    }else if(points >= KUDOS_TO_POINTS[0].value){
+      kudos.push(KUDOS_TO_POINTS[0].name);
+      points -= KUDOS_TO_POINTS[0].value;
+    }else{
+      points -= points;
+    }
+  }
+
+  return kudos;
+}
 
 /* 
   Recebe: Recebe um array contendo os nomes dos kudos de um usuário. Ex.: ['OK', 'GOOD']
   Retorna: a mensagem padrão com o valor em reais dos kudos por extenso. Ex.: Parabéns, você ganhou vinte e cinco reais
 */
-function getKudosValueMessageForUser(kudos) {}
+function getKudosValueMessageForUser(kudos) {
+  var msg;
+  var valor = 0;
+  var ind = 0;
+
+  for (let index = 0; index < kudos.length; index++) {
+    ind = KUDOS_TO_REAL.map(function(e) { return e.name; }).indexOf(kudos[index]);
+    valor += KUDOS_TO_REAL[ind].value;
+    if(index > 0){
+      kudos[index] = " " + kudos[index];
+    }
+  }
+
+  var valorExtenso = extenso(valor);
+  msg = "Você recebeu " + valorExtenso + " reais em retorno aos kudos " + kudos + "!";
+  
+  return msg;
+}
 
 module.exports = {
   getKudosForUser,
